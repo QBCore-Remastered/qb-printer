94945546f384e6e25d3c701affb390e878a34d80
Tony <97451137+TonybynMp4@users.noreply.github.com>
2022-11-06 22:34:25 +0100

    Facking hell i hate myself
    Co-authored-by: Yvan Cywan <c1yvan@outlook.com>

--- locales/fr.lua
+++ locales/fr.lua
@@ -1,6 +1,6 @@
 local Translations = {
     error = {
-        invalid_ext = "Ce n'est pas une extension valide, seul les liens utilisant les extensions {fileext} sont autorisées. ",
+        invalid_ext = "Ce n'est pas une extension valide, seul les liens utilisant les extensions %{fileext} sont autorisées. ",
     },
     info = {
         use_printer = "Utiliser l'imprimante"
